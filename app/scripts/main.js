6e693eac14a4f68ab7dd6b777a4895b58aa42d72
evatlsong <evatlsong@gmail.com>
2016-10-11 20:39:11 +0800

    change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,36 @@
+'use strict';
 $(function () {
+
+  //with no dependencies, it should be fine to use this instead of ondomcontentloaded
+  var a = document.createElement('audio');
+  if (!a.canPlayType) {
+    $("#support").append('Your browser does not support HTML5 Audio<br>');
+  }
+  if (!(a.canPlayType && a.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''))) {
+    $("#support").append('Your browser does not support Ogg Vorbis Playback<br>');
+  }
+
+  if (!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
+    $("#support").append('Your browser does not support MP3 Playback<br>');
+  }
+
+  var f = document.createElement('input');
+  f.type = 'file';
+  if (!('multiple' in f)) {
+    $("#support").append('Your browser does not support selecting multiple files<br>');
+  }
+  if (!('webkitdirectory' in f)) {
+    $("#support").append('Your browser probably does not support selecting directories<br>');
+  }
+  if (window.createObjectURL) {
+  } else if (window.createBlobURL) {
+  } else if (window.URL && window.URL.createObjectURL) {
+  } else if (window.webkitURL && window.webkitURL.createObjectURL) {
+  } else {
+    $("#support").append('Your browser probably does not support Object URLs<br>');
+  }
+
+  // init behavior
   $('#fileSelect input').on('change', function () {
     getSongs(this.files);
   });
@@ -6,4 +38,17 @@ $(function () {
     $('#fileSelect input').get(0).click();
     return false;
   });
+  var showVideo = function (src) {
+    var source = $('.modal video').get(0);
+    var url;
+    url = localStorage.getItem(src);
+    source.src = url;
+    source.play();
+    $('.modal').modal();
+  };
+  $('dl dd').on('click', 'a', function () {
+    showVideo($(this).attr('href'));
+    return false;
+  });
+
 });
--- app/scripts/video.js
+++ app/scripts/video.js
@@ -1,20 +1,5 @@
 'use strict';
 
-$(function () {
-  var showVideo = function (src) {
-    var source = $('.modal video').get(0);
-    var url;
-    url = localStorage.getItem(src);
-    source.src = url;
-    source.play();
-    $('.modal').modal();
-  };
-  $('dl dd').on('click', 'a', function () {
-    showVideo($(this).attr('href'));
-    return false;
-  });
-});
-
 function getUrl(file) {
   var url;
   if (window.createObjectURL) {
@@ -53,7 +38,6 @@ function getSongs(files) {
     var size = file.size || file.fileSize || 4096;
     if (size < 4095) {
       // Most probably not a real MP3
-      console.log(path);
       continue;
     }
 
@@ -70,40 +54,5 @@ function getSongs(files) {
 
     url = getUrl(file);
     localStorage.setItem(file.name, url);
-
   }
-
 }
-
-
-$(function () {
-  //with no dependencies, it should be fine to use this instead of ondomcontentloaded
-  var a = document.createElement('audio');
-  if (!a.canPlayType) {
-    $("#support").append('Your browser does not support HTML5 Audio<br>');
-  }
-  if (!(a.canPlayType && a.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''))) {
-    $("#support").append('Your browser does not support Ogg Vorbis Playback<br>');
-  }
-
-  if (!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
-    $("#support").append('Your browser does not support MP3 Playback<br>');
-  }
-
-  var f = document.createElement('input');
-  f.type = 'file';
-  if (!('multiple' in f)) {
-    $("#support").append('Your browser does not support selecting multiple files<br>');
-  }
-  if (!('webkitdirectory' in f)) {
-    $("#support").append('Your browser probably does not support selecting directories<br>');
-  }
-  if (window.createObjectURL) {
-  } else if (window.createBlobURL) {
-  } else if (window.URL && window.URL.createObjectURL) {
-  } else if (window.webkitURL && window.webkitURL.createObjectURL) {
-  } else {
-    $("#support").append('Your browser probably does not support Object URLs<br>');
-  }
-
-});
